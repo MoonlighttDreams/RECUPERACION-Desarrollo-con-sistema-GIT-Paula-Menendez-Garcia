e3142c1e318b39de77ae62de67cd287b67be9a81
MoonlighttDreams <165025799+MoonlighttDreams@users.noreply.github.com>
2024-05-28 20:52:15 +0200

    Monedas
    Añado monedas al juego

---
+++ unity game/proyecto 3d/Assets/Código/Moneda.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moneda : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(70 * Time.deltaTime, 0, 0);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") 
+        { 
+            Destroy(gameObject);
+            Debug.Log("Monedas:" + Panelgameover.numberOfCoins);
+            Panelgameover.numberOfCoins += 1;
+        }
+    }
+}
