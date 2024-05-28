edb18fc9e46d889dd755cd22d3f444c520a5acf4
MoonlighttDreams <165025799+MoonlighttDreams@users.noreply.github.com>
2024-05-28 21:02:49 +0200

    Texto Monedas
    Añado el texto de las monedas

--- unity game/proyecto 3d/Assets/Código/Panelgameover.cs
+++ unity game/proyecto 3d/Assets/Código/Panelgameover.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +13,7 @@ public class Panelgameover : MonoBehaviour
     public static bool gameOver;
     public GameObject gameOverPanel;
     public static int numberOfCoins;
+    public Text TextoMonedas;
 
     void Start()
     {
@@ -30,7 +32,7 @@ public class Panelgameover : MonoBehaviour
             GameStarted = true;
             Destroy(textoEmpezar);
         }
-
+        TextoMonedas.text = "Monedas:" + numberOfCoins;
         if (gameOver)
         {
             Time.timeScale = 0;
