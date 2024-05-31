24150bd39fc8982a34b301ecd49c4ce5cc9669f5
MoonlighttDreams <165025799+MoonlighttDreams@users.noreply.github.com>
2024-05-31 20:48:48 +0200

    Hub Tienda
    Hub de la tienda hecho

---
+++ unity game/proyecto 3d/Assets/Código/Rotador.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotador : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 50 * Time.deltaTime, 0);
+    }
+}
