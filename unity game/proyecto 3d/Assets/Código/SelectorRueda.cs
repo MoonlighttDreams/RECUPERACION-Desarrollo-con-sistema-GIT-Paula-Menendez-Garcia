1975a3b35f3a2519ff7cfb715764f4c0bfd30640
MoonlighttDreams <165025799+MoonlighttDreams@users.noreply.github.com>
2024-05-31 23:46:37 +0200

    Comprar 1

---
+++ unity game/proyecto 3d/Assets/Código/SelectorRueda.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorRueda : MonoBehaviour
+{
+    public GameObject[] Ruedas;
+    public int CurrentModelo = 0;
+
+
+    void Start()
+    {
+        CurrentModelo = PlayerPrefs.GetInt("Selected", 0);
+        foreach (GameObject rueda in Ruedas)
+            rueda.SetActive(false);
+
+        Ruedas[CurrentModelo].SetActive(true);
+    }
+}
